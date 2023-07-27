3bedc3e427dad91673649be47f5b740430dec1ff
majikina131012 <majikina.ryuma.infratop@gmail.com>
2023-07-27 07:47:06 +0000

    一回目提出

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,13 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @genres = Genre.all
+    
+    if params[:genre_id].present?
+      @items = Item.where(genre_id: params[:genre_id])
+    else
+      @items = Item.all
+    end  
+    
   end
   
   def show
